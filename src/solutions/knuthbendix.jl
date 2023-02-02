469d597c7eaab632159edb96cbaf8a6955666460
Marek Kaluba <kalmar@amu.edu.pl>
2023-02-02 11:22:32 +0100

    add reduction for rewriting systems

--- src/solutions/knuthbendix.jl
+++ src/solutions/knuthbendix.jl
@@ -54,7 +54,66 @@ function resolve_overlaps!(
     return rws
 end
 
-function reduce(rws::RewritingSystem)
-    @warn "not implemented yet"
-    return rws
+function reduce(rws::RewritingSystem{W}) where {W}
+    p = irreduciblesubsystem(rws)
+    R = empty(rws)
+    for lside in p
+        push!(R, lside, rewrite(lside, rws))
+    end
+    return R
+end
+
+"""
+    irreduciblesubsystem(rws::RewritingSystem)
+Return an array of left sides of rules from rewriting system of which all the
+proper subwords are irreducible with respect to this rewriting system.
+"""
+function irreduciblesubsystem(rws::RewritingSystem{W}) where {W}
+    lsides = W[]
+    for rule in rwrules(rws)
+        lhs, _ = rule
+        irreducible = true
+        if length(lhs) >= 2
+            for sw in subwords(lhs, 1, length(lhs) - 1) # proper subwords
+                if !isirreducible(sw, rws)
+                    @debug "subword $sw of $lhs is reducible. skipping!"
+                    irreducible = false
+                    break
+                end
+            end
+        end
+        if irreducible
+            push!(lsides, lhs)
+        end
+    end
+    return unique!(lsides)
+end
+
+function Base.occursin(subword::AbstractWord, word::AbstractWord)
+    n = length(subword)
+    n > length(word) && return false
+    for i in 0:length(word)-n
+        found = true
+        for j in 1:n
+            if subword[j] ≠ word[i+j]
+                found = false
+                break
+            end
+        end
+        if found
+            return true
+        end
+    end
+    return false
+end
+
+function isirreducible(w::AbstractWord, rws::RewritingSystem)
+    return !any(r -> occursin(first(r), w), rwrules(rws))
+end
+
+function subwords(w::AbstractWord, minlength = 1, maxlength = length(w))
+    n = length(w)
+    return (
+        w[i:j] for i in 1:n for j in i:n if minlength <= j - i + 1 <= maxlength
+    )
 end
