9e0a89f5f23fbab659fca54a4f35f627bb0fff81
Marek Kaluba <kalmar@amu.edu.pl>
2023-03-01 14:51:08 +0100

    load transversals before schreier_sims

--- src/CGT_KIT_WS2022.jl
+++ src/CGT_KIT_WS2022.jl
@@ -16,7 +16,6 @@ end
 include("AbstractPermutations.jl")
 include("Permutations.jl")
 include("orbit_plain.jl")
-
-include("schreier_sims.jl")
 include("transversals.jl")
+include("schreier_sims.jl")
 end # module CGT_KIT_WS2022
